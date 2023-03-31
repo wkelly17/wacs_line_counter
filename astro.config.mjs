209199a61eacef2e617539e0cbedfabb318c9bff
Will Kelly <willkelly@Will-Kellys-Pro.local>
2023-03-31 13:06:58 -0500

    edited astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 import solidJs from "@astrojs/solid-js";
+import {defineConfig} from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+
+import UnoCSS from "unocss/astro";
+import {presetUno} from "unocss";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import presetIcons from "@unocss/preset-icons";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
-    mode: "advanced"
+    mode: "advanced",
   }),
-  integrations: [solidJs()]
+  integrations: [
+    solidJs(),
+    UnoCSS({
+      presets: [presetUno(), presetIcons({})],
+      transformers: [transformerVariantGroup()],
+    }),
+  ],
 });
