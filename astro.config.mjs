3611cc4bb669b3b8d7bc5ae5e872da563448dbbf
Will Kelly <willkelly@Will-Kellys-Pro.local>
2023-03-31 13:17:07 -0500

    changing back to uno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import {defineConfig} from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
-// import UnoCSS from "unocss/astro";
-// import {presetUno} from "unocss";
-// import transformerVariantGroup from "@unocss/transformer-variant-group";
-// import presetIcons from "@unocss/preset-icons";
+import UnoCSS from "unocss/astro";
+import {presetUno} from "unocss";
+import presetIcons from "@unocss/preset-icons";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
-    mode: "directory",
+    mode: "advanced",
   }),
-  integrations: [solidJs()],
+  integrations: [
+    solidJs(),
+    UnoCSS({
+      presets: [presetUno(), presetIcons({})],
+    }),
+  ],
 });
